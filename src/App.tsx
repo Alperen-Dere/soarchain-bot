db3cc45e6c2e6514bb8c18cb5c6164d6b1c919b3
Alperen-Dere <alperen-dere@hotmail.com>
2024-07-03 15:19:47 +0300

    refactor: refactor scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import { soarchainLogo, dollarCoin, telegram, twitter } from './images'; // Ensure these images are imported correctly
 import Info from './icons/Info';
@@ -28,15 +28,12 @@ const App: React.FC = () => {
     }
   };
 
-  const [tasks
-  ] = useState([
+  const [tasks] = useState([
     { id: 1, title: 'Join our TG channel', reward: 5000, completed: false, details: 'Become part of our community to stay updated with all the latest news and developments.', link: 'https://t.me/soarchain', verified: false, icon: telegram },
     { id: 2, title: 'Follow our X account', reward: 5000, completed: false, details: 'Follow our official account, retweet the airdrop announcement, and tag a friend. Help us spread the word and grow our community!', link: 'https://twitter.com/soarchain', verified: false, icon: twitter },
     { id: 3, title: 'Complete the Registration Form', reward: 10000, completed: false, details: 'Provide your details through our form to ensure you’re eligible for token distribution. Make sure you enter correct information for seamless participation.', link: '#', verified: false, icon: soarchainLogo },
     { id: 4, title: 'Invite Friends', reward: 25000, completed: false, details: 'Invite your friends to join Soarchain and earn rewards when they sign up using your referral link.', link: '', verified: false, icon: soarchainLogo }
   ]);
-
-
 
   const handleInviteFriends = () => {
     if (user) {
@@ -46,6 +43,11 @@ const App: React.FC = () => {
       );
     }
   };
+
+  // Scroll to top when page changes
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [currentPage]);
 
   return (
     <div className="bg-black flex justify-center">
@@ -85,7 +87,7 @@ const App: React.FC = () => {
           </div>
         </div>
 
-        <div className="flex-grow mt-4 bg-[#8e2de2] rounded-t-[48px] relative top-glow z-0">
+        <div className="flex-grow mt-4 bg-[#8e2de2] rounded-t-[48px] relative top-glow z-0 overflow-auto">
           <div className="absolute top-[2px] left-0 right-0 bottom-0 bg-[#1d2025] rounded-t-[46px] overflow-auto">
             {currentPage === 'home' && (
               <div className="px-4 mt-4 task-list">
